b26839d7d81f66f9e46f16624b0c7d8e550ed598
Maksim Vladimirov <m.vladimirov@qpdev.ru>
2026-05-17 17:40:41 +0300

    feat: noindex for pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "MVXIII by Maxim Vladimirov",
   description: "Откройте нашу коллекцию костюмов, рубашек и аксессуаров.",
+  robots: { index: false, follow: false },
 };
 
 export default function RootLayout({
